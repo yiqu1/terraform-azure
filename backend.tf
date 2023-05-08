4f4d918ca925a7ce2ef45075293f8ce16296706e
Yi Qu <yqu82888@gmail.com>
2023-05-08 14:15:01 -0400

    added files to repo
    Co-Authored-By: Jaspal Singh <43149034+jass85688@users.noreply.github.com>

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    storage_account_name = "storageaccont412"      # Use your own unique name here
+    container_name       = "tf-storage-container-0" # Use your own container name here
+    key                  = "terraform.tfstate"      # Add a name to the state file
+    resource_group_name  = "terraform-sample"       # Use your own resource group name here
+  }
+}
