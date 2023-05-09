c6f62e18bf1be573bb094124177a2bb86be1f0f9
Yi Qu <yqu82888@gmail.com>
2023-05-09 15:23:27 -0400

    changes
    Co-Authored-By: Jaspal Singh <43149034+jass85688@users.noreply.github.com>

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "azurerm" {
-#     storage_account_name = "storageaccont41111"      # Use your own unique name here
-#     container_name       = "tf-storage-container-0" # Use your own container name here
-#     key                  = "terraform.tfstate"      # Add a name to the state file
-#     resource_group_name  = "terraform-sample"       # Use your own resource group name here
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    storage_account_name = "storageaccont41111"      # Use your own unique name here
+    container_name       = "tf-storage-container-0" # Use your own container name here
+    key                  = "terraform.tfstate"      # Add a name to the state file
+    resource_group_name  = "backend-rg"       # Use your own resource group name here
+  }
+}
