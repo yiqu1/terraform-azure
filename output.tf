95a2a668abe461edf017e954f5ba1cd3ebab9bfa
jass85688 <jaspalsinghsarinh@gmail.com>
2023-05-10 09:18:26 -0400

    updated output.tf

--- output.tf
+++ output.tf
@@ -6,9 +6,6 @@ output "backend_id" {
   value = azurerm_subnet.my_subnet_backend.id
 }
 
-output "dmz_id" {
-  value = azurerm_subnet.my_subnet_dmz.id
-}
 
 output "load_balancer_ip" {
   value = azurerm_public_ip.frontend.ip_address
